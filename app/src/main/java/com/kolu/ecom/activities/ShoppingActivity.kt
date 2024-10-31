8cc15138e6ef5c6ebcfc4d48a981faa8ee454b16
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-10-31 08:17:30 +0600

    created all the necessary packages

--- app/src/main/java/com/kolu/ecom/activities/ShoppingActivity.kt
+++ app/src/main/java/com/kolu/ecom/activities/ShoppingActivity.kt
@@ -1,25 +1,21 @@
-package com.kolu.ecom
+package com.kolu.ecom.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.kolu.ecom.databinding.ActivityMainBinding
+import com.kolu.ecom.R
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(binding.root)
+        setContentView(R.layout.activity_shopping)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
     }
 }
