a3ac91ff9381f5a8fe5eef23b3e02068e7d20e21
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2024-11-03 11:13:18 +0600

    unnecessary commit.

--- app/src/main/java/com/kolu/ecom/fragments/loginregister/AccountOptionsFragment.kt
+++ app/src/main/java/com/kolu/ecom/fragments/loginregister/AccountOptionsFragment.kt
@@ -3,5 +3,5 @@ package com.kolu.ecom.fragments.loginregister
 import androidx.fragment.app.Fragment
 import com.kolu.ecom.R
 
-class AccountOptionsFragment: Fragment(R.layout.fragment_acount_option) {
+class AccountOptionsFragment: Fragment(R.layout.fragment_account_option) {
 }
